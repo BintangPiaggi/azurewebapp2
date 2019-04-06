5ffb99f34aec51e04e6cc5adf8e6e87a195a0204
Bintang Piaggi Putra <41603399+BintangPiaggi@users.noreply.github.com>
2019-04-06 18:51:43 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -16,7 +16,7 @@ if (isset($_POST['submit'])) {
 <html>
 <head>
 	<title>Bintang Submision 2</title>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
 	<script type="text/javascript">
